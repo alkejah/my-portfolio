ffe5f61392743573c7db8f732ef2564b5e6064c7
alkejah <richard.delapena19@gmail.com>
2026-09-11 09:54:53 +0800

    adjusted pricing ui padding

--- src/components/sections/pricing-section.tsx
+++ src/components/sections/pricing-section.tsx
@@ -19,7 +19,7 @@ const pricingPlans = [
     icon: GraduationCap,
     features: [
       "Responsive website",
-      "Up to 10 pages",
+      "Up to 15 pages",
       "Basic deployment",
       "Email support",
     ],
